ab596adeac85afccd1236874babe1c0878abe22d
sofiiman2025-cpu <sofiiman2025@gmail.com>
2026-06-07 21:34:33 -0300

    .

--- pryAcademiaManassero/frmCargaPlan.Designer.cs
+++ pryAcademiaManassero/frmCargaPlan.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
-            this.ClientSize = new System.Drawing.Size(632, 421);
+            this.ClientSize = new System.Drawing.Size(545, 347);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmCargaPlan";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
